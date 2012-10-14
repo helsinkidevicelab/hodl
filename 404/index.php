d2865177697ccf29f6c2b26aa6675251d5ea27bb
Viljami S <viljami.salminen@gmail.com>
2012-10-14 13:47:49 +0300

    Add absolute paths

--- 404/index.php
+++ 404/index.php
@@ -7,12 +7,12 @@ include($_SERVER["DOCUMENT_ROOT"]."/includes/header.php");
   </head>
   <body>
     <div class="header">
-      <img src="img/logo_packed.png" class="logo" alt="Logo">
-      <img src="img/office-sml.jpg?full=img/office-mdm.jpg" class="illustration" alt="Helsinki Open Device Lab">
+      <img src="/img/logo_packed.png" class="logo" alt="Logo">
+      <img src="/img/office-sml.jpg?full=/img/office-mdm.jpg" class="illustration" alt="Helsinki Open Device Lab">
       <div class="wrapper">
         <div class="m_wrap">
           <div class="nav">
-            <img src="img/logo_packed.png" class="logo sec" alt="Logo">
+            <img src="/img/logo_packed.png" class="logo sec" alt="Logo">
             <h1><b>Helsinki</b> <i>open device lab</i></h1>
             <a href="http://opendevicelab.com/" class="button">Go to front page</a>
           </div>
