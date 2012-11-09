bdaf4cd9d489c5243fa0f51f20e13b0c762e5be5
Viljami S <viljami.salminen@gmail.com>
2012-11-09 13:01:16 +0200

    Change the order

--- index.php
+++ index.php
@@ -184,7 +184,7 @@ include("includes/header.php");
             <tr>
               <td data-title="Device">Nintendo Game Boy</td>
               <td data-title="OS">Game Boy OS</td>
-              <td data-customkey="1" data-title="Screen Size">160&nbsp;x&nbsp;144</td>
+              <td data-customkey="0" data-title="Screen Size">160&nbsp;x&nbsp;144</td>
               <td data-title="Contributor"><a href="http://twitter.com/viljamis" rel="external">Viljami S.</a></td>
             </tr>
             <tr>
